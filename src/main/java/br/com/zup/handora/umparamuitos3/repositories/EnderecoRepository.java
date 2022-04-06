82a0c95e4727837da4cfe126303be7fc5620f6b6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-06 20:47:45 -0300

    Add Endereco repository

---
+++ src/main/java/br/com/zup/handora/umparamuitos3/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.umparamuitos3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.umparamuitos3.models.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
