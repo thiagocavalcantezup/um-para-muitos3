67194e4bcb43e2cdb67357ecfc2f83df6f44d8b8
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-06 19:33:09 -0300

    Add repository for Zupper

---
+++ src/main/java/br/com/zup/handora/umparamuitos3/repositories/ZupperRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.umparamuitos3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.umparamuitos3.models.Zupper;
+
+public interface ZupperRepository extends JpaRepository<Zupper, Long> {
+
+}
