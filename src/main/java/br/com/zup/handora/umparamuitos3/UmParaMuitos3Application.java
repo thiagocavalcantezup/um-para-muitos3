cb6c8cc670a59c7962a37dec88662cb9273ca1aa
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-06 19:13:05 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/umparamuitos3/UmParaMuitos3Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.umparamuitos3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmParaMuitos3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmParaMuitos3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/handora/umparamuitos3/UmParaMuitos3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.umparamuitos3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmParaMuitos3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
