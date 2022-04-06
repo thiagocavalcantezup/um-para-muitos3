3b2ee0e1afdf7eb62ff152dcdedb8222721bd02e
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-06 19:41:57 -0300

    Add method to create a new Zupper in controller

--- src/main/java/br/com/zup/handora/umparamuitos3/controllers/ZupperController.java
+++ src/main/java/br/com/zup/handora/umparamuitos3/controllers/ZupperController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.umparamuitos3.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.umparamuitos3.models.Zupper;
+import br.com.zup.handora.umparamuitos3.models.ZupperDTO;
 import br.com.zup.handora.umparamuitos3.repositories.ZupperRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class ZupperController {
 
     public ZupperController(ZupperRepository zupperRepository) {
         this.zupperRepository = zupperRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid ZupperDTO zupperDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Zupper zupper = zupperRepository.save(zupperDTO.toModel());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(zupper.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
